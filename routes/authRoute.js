ad18d557da51df2c28668ada5076570acec2c903
minipekka25 <63054647+minipekka25@users.noreply.github.com>
2020-09-09 21:54:00 +0530

    sdf

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,7 +7,7 @@ module.exports = (app)=>{
 }))
 
 app.get("/auth/google/callback",passport.authenticate('google'),(req,res)=>{
-    res.redirect('/orgexplorer')
+    res.redirect('http://localhost:5000/orgexplorer')
 })
 
 app.get('/api/current_user',(req,res)=>{
