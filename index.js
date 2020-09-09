3f1240821af845a5c682e2623a4571da4b1dac44
minipekka25 <63054647+minipekka25@users.noreply.github.com>
2020-09-09 18:50:13 +0530

    dvbf

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
     const path = require('path');
     app.get('*',(req,res)=>{
+        res.setHeader("Content-Type", "text/html");
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
