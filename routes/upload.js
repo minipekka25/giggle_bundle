394c1bc1760312b44d5b4e0c9d0b99557bfb0850
minipekka25 <63054647+minipekka25@users.noreply.github.com>
2020-09-09 12:30:51 +0530

    ADF

--- routes/upload.js
+++ routes/upload.js
@@ -5,15 +5,15 @@ const path = require('path');
 
 
 const s3 = new aws.S3({
-    accessKeyId: "AKIATDPBLE7GKEX52RWM",
-    secretAccessKey: "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",
-    Bucket: "slackclonetesting",
+    accessKeyId: process.env.S3_ACCESS_ID,
+    secretAccessKey: process.env.S3_ACCESS_KEY,
+    Bucket: process.env.S3_BUCKET_NAME
 });
 
 var upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: "slackclonetesting",
+        bucket: process.env.S3_BUCKET_NAME,
         key: function (req, file, cb) {
             cb(null, path.basename(file.originalname, path.extname(file.originalname)) + '__--__' + Date.now() + path.extname(file.originalname))
         }
